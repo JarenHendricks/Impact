d495f8e2ff1b4999788ccdae8a92c6f622880d6a
JarenHendricks <jarenhendricks@gmail.com>
2021-02-17 18:08:20 +0200

    Updated Main class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Collection;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Worlds");
+        RangeSummarizer rangeSum = new RangeSummarizer();
+        Collection<Integer> array = rangeSum.collect("0,2,3,4,6,8,9");
+        System.out.println(rangeSum.summarizeCollection(array));
     }
 }
